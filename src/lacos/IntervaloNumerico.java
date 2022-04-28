254ae9720cce91b94fe052032b7c558a42222c32
Anderson Freires <dre.son36@gmail.com>
2022-04-28 15:34:49 -0300

    Program that presents the results of the sum and arithmetic mean of even values located in the numerical range from 50 to 70.

---
+++ src/lacos/IntervaloNumerico.java
@@ -0,0 +1,20 @@
+package lacos;
+
+public class IntervaloNumerico {
+public static void main(String[] args) {
+	IntervaloNumerico.cal();
+}
+
+public static void cal() {
+	int cont=0,soma=0,media=0;
+	
+	for(int i=50;i<=70;i++) {
+		if(i % 2==0) {
+			soma+=i;
+			cont++;
+		}
+		media=soma/cont;
+	}
+	System.out.println("Quantidade: "+cont+ "\nsoma= "+soma+"\nMedia= "+media);
+}
+}
